cb591d2360cb3b9784fd63d9db35cb1dd1f6a2ed
Matheus Eiji Faria Komatsu <matheuskomatsu@hotmail.com>
2024-04-11 16:20:49 -0300

    Incluído template para inserção de eventos da agena. Incluído gitignore

--- core/views.py
+++ core/views.py
@@ -15,6 +15,11 @@ def list_eventos(request):
     dados = {'eventos': evento}
     return render(request, 'agenda.html',dados)
 
+
+@login_required(login_url='/login/')
+def evento(request):
+    return render(request,'evento.html')
+
 def login_user(request):
     return render(request, 'login.html')
 
@@ -29,6 +34,19 @@ def submit_login(request):
         else:
             messages.error(request, "Usuário ou senha inválido")
     return redirect('/')
+
+def submit_evento(request):
+    if request.POST:
+        titulo = request.POST.get('titulo')
+        data_evento = request.POST.get('data_evento')
+        descricao = request.POST.get('descricao')
+        usuario = request.user
+        Evento.objects.create(titulo=titulo,
+                              data_evento=data_evento,
+                              descricao=descricao,
+                              usuario=usuario)
+    return redirect('/')
+
 
 def logout_user(request):
     logout(request)
