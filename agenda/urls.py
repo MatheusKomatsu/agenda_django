942a275d361109cf34c678ae14ab6b59f1d08b93
Matheus Eiji Faria Komatsu <matheuskomatsu@hotmail.com>
2024-04-11 18:01:40 -0300

    Adicionado funções de alteração e exclusão de dados

--- agenda/urls.py
+++ agenda/urls.py
@@ -25,9 +25,11 @@ urlpatterns = [
     path('agenda/', views.list_eventos),
     path('agenda/evento/', views.evento),
     path('agenda/evento/submit', views.submit_evento),
+    path('agenda/evento/delete/<int:id_evento>/', views.delete_evento),
     path('', RedirectView.as_view(url='/agenda/')),
     path('login/', views.login_user),
     path('login/submit', views.submit_login), # / no final é resevado para método get
     path('logout/', views.logout_user),
+
 
 ]
